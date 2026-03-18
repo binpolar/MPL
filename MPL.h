c698d91e363fb80944d4149f98eac7b399558966
Dario Cardajoli <dario.cardajoli@monozukuri.eu>
2026-03-18 11:20:36 +0100

    modified:   MPL.h

--- MPL.h
+++ MPL.h
@@ -7,15 +7,12 @@
 struct mpl_node_t;
 struct mpl_route_t;
 
-void mpl_init(void); //resets nw data structures
+void mpl_init(void); // resets nw data structures
 
 bool mpl_create_edge_if_not_exist(struct mpl_node_t *node1, struct mpl_node_t *node2, uint8_t quality);
 
 bool mpl_create_node_if_not_exist(uint32_t addr1, uint32_t addr2, uint8_t quality);
 
 bool mpl_find_route(uint32_t src_addr, uint32_t dest_addr, struct mpl_route_t *route);
-
-
-
 
 #endif
